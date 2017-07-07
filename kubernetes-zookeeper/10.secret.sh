87eb5e82623b572eef7a8644ee3422be15f2c324
triumph2008 <triumph2008@gmail.com>
2017-07-07 15:11:50 +0800

    add k8s zookeeper

--- kubernetes-zookeeper/10.secret.sh
+++ kubernetes-zookeeper/10.secret.sh
@@ -1,1 +1,5 @@
-kubectl create secret generic ceph-secret-admin --from-literal=key='AQADMr1YXvIXHhAAoXSLnmgXhjHeXS01gEIQdg=='  --type=kubernetes.io/rbd
+ceph auth get-key client.admin
+
+kubectl create secret generic ceph-secret-admin --from-literal=key='AQADMr1YXvIXHhAAoXSLnmgXhjHeXS01gEIQdg=='  --type=kubernetes.io/rbd  --namespace=kube-system
+
+kubectl create secret generic ceph-secret-admin --from-literal=key='AQADMr1YXvIXHhAAoXSLnmgXhjHeXS01gEIQdg=='  --type=kubernetes.io/rbd  --namespace=default
